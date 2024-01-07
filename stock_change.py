0afd9aa0473784e67f2b8678dcc3185da707371c
Rustam1616 <data.rustam.nazarov@gmail.com>
2024-01-07 12:15:11 +0400

    1

--- stock_change.py
+++ stock_change.py
@@ -34,7 +34,7 @@ firms = ['AAPL','MSFT','AMZN','NVDA','GOOGL','META','GOOG','BRK-B','TSLA','UNH',
          'BXP','FOXA','CTLT','GNRC','TPR','BEN','FRT','FMC','BBWI','PARA','XRAY','IVZ','BIO','NCLH','WHR','CMA','HAS',
          'VFC','ZION','DVA','RL','SEE','ALK','SEDG','MHK','FOX','NWS']
 gdf = pd.DataFrame(columns=['Date','Close','Company','roll','percent', '3 days ago'])
-st.slider('Sample size', min_value = 51, max_value = len(firms)-1)
+st.slider('Sample size', min_value = 51, max_value = 500)
 for firm in sample(firms, 51):
     ticker = yf.Ticker(firm)
     df = ticker.history(start = datetime.datetime.today()-datetime.timedelta(days=5), 
